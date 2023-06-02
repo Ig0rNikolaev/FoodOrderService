c2f5e89f8764a570c5503c4ba3af7b894dfd5586
Igor Nikolaev <igor01131987@gmail.com>
2023-06-02 15:10:43 +0300

    Add start setup.

---
+++ FoodOrderService/Sources/App/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  FoodOrderService
+//
+//  Created by Игорь Николаев on 02.06.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let viewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ FoodOrderService/Sources/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  FoodOrderService
+//
+//  Created by Игорь Николаев on 02.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //: MARK: - UI Elements
+
+
+    //: MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupHierarchy()
+        setupLayout()
+    }
+
+    //: MARK: - Setups
+
+    private func setupView() {
+
+    }
+
+    private func setupHierarchy() {
+
+
+    }
+
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+
+        ])
+    }
+}
+
